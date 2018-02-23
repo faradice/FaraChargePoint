e14fe44ed9bb100c76e4fc3f79b60c326dcefb57
ragnarvald <ragnar@decode.is>
2018-02-23 19:23:45 +0000

    OCPP refacoring

--- test/com/faradice/ocpp/test/FaraChargePointCentralTest.java
+++ test/com/faradice/ocpp/test/FaraChargePointCentralTest.java
@@ -2,15 +2,12 @@ package com.faradice.ocpp.test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.List;
 
 import javax.xml.namespace.QName;
-import javax.xml.ws.BindingProvider;
 import javax.xml.ws.Service;
-import javax.xml.ws.handler.Handler;
-import javax.xml.ws.soap.AddressingFeature;
 
-import com.faradice.ocpp.OcppHeaderHandler;
+import com.faradice.ocpp.OcppFactory;
+
 /* 1.5
 import ocpp.cs._2012._06.IdTagInfo;
 import ocpp.cs._2012._06.AuthorizeRequest;
@@ -41,30 +38,17 @@ public class FaraChargePointCentralTest {
 	static String sericeName = "CentralSystemService";
 */
 	
-
+/*
 //  OCPP 15 Faradice DO server
 //	http://104.236.81.197:8088/Ocpp15WebAppDemo/CentralSystemService?wsdl	
 	static String endpoint = "http://104.236.81.197:8088/Ocpp15WebAppDemo/CentralSystemService";
 	static String uri = endpoint+"?wsdl";
 	static String serviceURN = "urn://Ocpp/Cs/2012/06/";
 	static String sericeName = "CentralSystemService";
-	
+	*/
 
 	
 /*
-//  OCPP 15 Faradice local netbeans server
-//	http://localhost:8080/Ocpp15WebAppDemo/CentralSystemService?wsdl	
-	static String endpoint = "http://localhost:8080/Ocpp15WebAppDemo/CentralSystemService";
-	static String uri = endpoint+"?wsdl";
-	static String serviceURN = "urn://Ocpp/Cs/2012/06/";
-	static String sericeName = "CentralSystemService";
-*/
-	
-	
-
-
-	
-
 /*	
 //  Direct local server
 	static String endpoint = "http://localhost:8085/Fara_occp/CentralSystemService";
@@ -87,30 +71,9 @@ public class FaraChargePointCentralTest {
 		System.out.println("Authentication from server: "+tagInfo.getStatus().value());
 	}
 
-	public static void addHandler() {
-		OcppHeaderHandler handler = new OcppHeaderHandler("Faradice1");
-		BindingProvider bp = (BindingProvider) ss;
-		List<Handler> handlerChain = bp.getBinding().getHandlerChain();
-		handlerChain.add(handler);
-		bp.getBinding().setHandlerChain(handlerChain);
-		System.out.println("Handler added: "+handler.getClass().getName());
-	}
-	
-	public static void initSoap() {
-		try {
-			url = new URL(uri);
-			qName = new QName(serviceURN, sericeName);
-			service = Service.create(url, qName);
-			AddressingFeature af = new AddressingFeature();
-			ss = service.getPort(CentralSystemService.class, af);
-			addHandler();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 
 	public static void main(String[] args) throws MalformedURLException {
-		initSoap();
+		ss = OcppFactory.digoCentralService16();
 		testAuthorizeRequest();
 	}
 
