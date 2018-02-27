935e45bb9fc5e437f8b464f25ac15f82668552e2
vt_faradice_rv <vt_faradice_rv@ONCORE-TEST.or.is>
2018-02-27 11:36:40 +0000

    Refactored OCPP16 / OCPP15 support

--- src/com/faradice/ocpp/Ocpp16HeaderHandler.java
+++ src/com/faradice/ocpp/Ocpp16HeaderHandler.java
@@ -2,21 +2,18 @@ package com.faradice.ocpp;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.UUID;
 import java.util.logging.Logger;
 
 import javax.xml.namespace.QName;
 import javax.xml.soap.SOAPElement;
 import javax.xml.soap.SOAPEnvelope;
 import javax.xml.soap.SOAPHeader;
-import javax.xml.soap.SOAPHeaderElement;
 import javax.xml.soap.SOAPMessage;
 import javax.xml.ws.handler.MessageContext;
 import javax.xml.ws.handler.soap.SOAPHandler;
 import javax.xml.ws.handler.soap.SOAPMessageContext;
 
 import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
-import com.sun.xml.internal.ws.developer.JAXWSProperties;
 
 public class Ocpp16HeaderHandler implements SOAPHandler<SOAPMessageContext> {
 	final static Logger log = Logger.getLogger(Ocpp16HeaderHandler.class.getName());
@@ -80,7 +77,7 @@ public class Ocpp16HeaderHandler implements SOAPHandler<SOAPMessageContext> {
 
 	public Set<QName> getHeaders() {
 		Set<QName> qns = new HashSet<>();
-		qns.add(new QName("urn://Ocpp/Cs/2012/06/","chargeBoxIdentity"));
+		qns.add(new QName("urn://Ocpp/Cs/2015/10/","chargeBoxIdentity"));
 		return qns;
 	}
 
