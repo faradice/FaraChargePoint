5d393748dfcd7759a09cc35d743fcafdeba1510c
ragnarvald <ragnar@decode.is>
2018-02-28 00:24:13 +0000

    cleanup

--- src/com/faradice/ocpp/centralsystem/CentralSystemMockupServer.java
+++ src/com/faradice/ocpp/centralsystem/CentralSystemMockupServer.java
@@ -8,11 +8,8 @@ import javax.xml.ws.handler.Handler;
 import com.faradice.ocpp.Ocpp16HeaderHandler;
 
 /*
- * Skoða þennan betur.  Virðist góður
- * https://www.javaworld.com/article/3215966/java-language/web-services-in-java-se-part-2-creating-soap-web-services.html
- * 
  * create classes from wsdl from cmd line wsimport tool:
- *   wsimport -keep -extension  -keep -Xendorsed -s src wsdl/OCPP_CentralSystemService_1.6.wsdl
+ *   wsimport -keep -extension -keep -Xendorsed -s src wsdl/OCPP_CentralSystemService_1.6.wsdl
  *   nb command line: 
  *   wsimport/Library/Java/JavaVirtualMachines/jdk1.8.0_65.jdk/Contents/Home/jre/bin/java -d /Users/ragnar/Ocpp2/build/generated/jax-wsCache/OCPP_CentralSystemService_1.6 -extension -Xendorsed -keep -s /Users/ragnar/Ocpp2/build/generated/jax-wsCache/OCPP_CentralSystemService_1.6 -encoding UTF-8 -verbose -Xnocompile -catalog /Users/ragnar/Ocpp2/catalog.xml /Users/ragnar/Ocpp2/src/conf/xml-resources/web-service-references/OCPP_CentralSystemService_1.6/wsdl/OCPP_CentralSystemService_1.6.wsdl -p ocpp2 -wsdllocation file:/Users/ragnar/FaraChargePoint/wsdl/OCPP_CentralSystemService_1.6.wsdl
  *
