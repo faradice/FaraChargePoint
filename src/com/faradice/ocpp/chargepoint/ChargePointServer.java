2204541255c28180d5f7bcb46aa683f058d78a31
ragnarvald <ragnar@decode.is>
2018-04-23 14:30:57 +0000

    hostname

--- src/com/faradice/ocpp/chargepoint/ChargePointServer.java
+++ src/com/faradice/ocpp/chargepoint/ChargePointServer.java
@@ -2,15 +2,18 @@ package com.faradice.ocpp.chargepoint;
 
 import javax.xml.ws.Endpoint;
 
+import com.faradice.faranet.FaraHttp;
+
 /*
  * 
  * create classes from wsdl from cmd line wsimport tool:
- *   wsimport -keep -extension  -Xendorsed -s src wsdl/OCPP_ChargePointService_1.6.wsdl
+ * wsimport -keep -extension  -Xendorsed -s src wsdl/OCPP_ChargePointService_1.6.wsdl
  */
 public class ChargePointServer {
 	public static void main(String[] args) {
-		String host = "localhost";
-		String port = "8069";
+		String ipAddress = FaraHttp.ipAddress();
+		String host = ipAddress;
+		String port = "8080";
 		String serviceName = "FaraChargePoint";
 		// Address to access wsdl file in browser
 		System.out.println(host+":"+port+"/"+serviceName+"?wsdl");
