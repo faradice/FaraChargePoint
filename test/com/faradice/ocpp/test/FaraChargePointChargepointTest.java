62c8556e3db41a2b0eb5ecca254869155cdf82b7
ragnarvald <ragnar@decode.is>
2018-07-12 10:04:09 +0000

    unittest

--- test/com/faradice/ocpp/test/FaraChargePointChargepointTest.java
+++ test/com/faradice/ocpp/test/FaraChargePointChargepointTest.java
@@ -16,7 +16,7 @@ public class FaraChargePointChargepointTest {
 	static ChargePointService cp = null;
 	
 	private static void initTest() throws Exception {
-		String host = "192.168.1.33";
+		String host = "192.168.1.177";
 		String port = "8080";
 		String serviceName = "FaraChargePoint";	
 		URL url = new URL("http://"+host+":"+port+"/"+serviceName+"/?wsdl");
@@ -44,7 +44,7 @@ public class FaraChargePointChargepointTest {
 	
 	public static void main(String[] args) throws Exception {
 		initTest();
-//		testStartTransaction();
+		testStartTransaction();
 //		Thread.sleep(60000);
 		testStopTransaction();
 	}
